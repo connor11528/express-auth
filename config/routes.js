9ef1b1484a5aa2290d1a047c309e593a4f2b1dbf
Connor James Leech <connorleech@gmail.com>
2014-02-02 18:02:08 +0300

    passport routing

--- config/routes.js
+++ config/routes.js
@@ -1,30 +1,44 @@
 module.exports = function(app, passport){
+	// home page
 	app.get('/', function(req, res){
 		res.render('../app/views/index.ejs');
 	});
 	
+	// login page
 	app.get('/login', function(req, res){
 		res.render('../app/views/login.ejs', { message: req.flash('loginMessage') });
 	});
 	
 	// process login form
-	app.post('/login', function(req, res){
-		// passport stuff
-	});
+	app.post('/login', passport.authenticate('local-login', {
+		successRedirect: '/profile',
+		failureRedirect: '/login',
+		failureFlash: true
+	}));
 	
+	// sign up page
 	app.get('/signup', function(req, res){
 		res.render('../app/views/signup.ejs', { message: req.flash('signupMessage') });
 	});
 	
 	// process signup form
-	app.post('/signup', function(req, res){
+	app.post('/signup', passport.authenticate('local-signup', {
+		successRedirect: '/profile',
+		failureRedirect: '/signup',
+		failureFlash: true
 		
-	});
+	}));
 	
+	// profile page
 	app.get('/profile', isLoggedIn, function(req, res){
 		res.render('../app/views/profile.ejs', {
 			user: req.user
 		});
+	});
+	
+	app.get('/logout', function(req, res){
+		req.logout();
+		res.redirect('/');
 	});
 };
 
